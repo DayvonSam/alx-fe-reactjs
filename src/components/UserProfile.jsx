2869c374d669e8f50c62e8c156f8adc2665f2bbe
Sam Anuro Omondi <sananuroomondi@gmail.com>
2025-11-30 19:04:59 +0300

    fix: exact classes and order for responsive design checker

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,16 +1,16 @@
 // src/components/UserProfile.jsx
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 sm:p-4 md:p-8 max-w-xs sm:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+    <div className="user-profile bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
-        className="w-24 h-24 sm:w-36 sm:h-36 rounded-full mx-auto"
+        className="w-24 h-24 md:w-36 md:h-36 rounded-full mx-auto"
       />
-      <h1 className="text-lg sm:text-xl text-blue-800 my-4 text-center font-semibold">
+      <h1 className="text-lg md:text-xl text-blue-800 my-4 text-center">
         John Doe
       </h1>
-      <p className="text-sm sm:text-base text-gray-600 text-center px-4">
+      <p className="text-sm md:text-base text-gray-600 text-center">
         Developer at Example Co. Loves to write code and explore new technologies.
       </p>
     </div>
